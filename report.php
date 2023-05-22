dd6adea0454a2bcbddea014af30d54a89f5f0d87
ShintaAmaliaA <126319663+ShintaAmaliaA@users.noreply.github.com>
2023-05-22 21:49:48 +0700

    Add files via upload

---
+++ report.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello WORD !');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello_word.xlsx');
+?>
